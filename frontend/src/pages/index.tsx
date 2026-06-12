6dac2eae89b1e821fb5f09fa07359d52cc950df7
Mitchell Collins <mitchell.collins@datapod.com.au>
2026-06-12 10:11:37 +1000

    Initial Commit

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>FlowForge</title>
+        <meta name="description" content="A project management website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
